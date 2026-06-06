83d537a0650538f79ca6982231d16ba09631b805
sarangi-shreya <shreyasarangi2006@gmail.com>
2026-06-06 13:33:54 +0530

    Add image processing features: implement deskew and process endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,9 @@
 import json
 import os
+import cv2
+import io
+import numpy as np
+from fastapi.responses import StreamingResponse
 from fastapi import FastAPI
 from fastapi import UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
@@ -10,7 +14,7 @@ app = FastAPI()     # creates the application instance. Every route is registere
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],
-    allow_methods=["*"],
+    allow_methods=["*"],              
     allow_headers=["*"],
 )
 os.makedirs("uploads", exist_ok=True)  
@@ -32,10 +36,81 @@ def save_metadata(filename, size, upload_time):
         "upload_time": upload_time 
     }
     existing_data.append(new_entry)
-
     with open("metadata.json", "w") as f:
         json.dump(existing_data, f, indent=4)
-            
+    
+def order_points(pts):
+    rect = np.zeros((4,2), dtype = "float32")
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+    return rect
+
+def deskew(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray, 75, 200)
+    contours, _ = cv2.findContours(edges, cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:5]
+    doc_contour = None
+    for c in contours:
+        epsilon = 0.02 * cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, epsilon, True)
+        if len(approx) == 4:
+            doc_contour = approx
+            break
+    if doc_contour is None:
+        return image  # return original if no document detected
+    
+    rect = order_points(doc_contour.reshape(4,2))
+    (tl, tr, br, bl) = rect
+    
+    widthA = np.sqrt((br[0]-bl[0])**2 + (br[1]-bl[1])**2)
+    widthB = np.sqrt((tr[0]-tl[0])**2 + (tr[1]-tl[1])**2) 
+    maxWidth = max(int(widthA),int(widthB))
+
+    heightA = np.sqrt((tr[0]-br[0])**2 + (tr[1]-br[1])**2)
+    heightB = np.sqrt((tl[0]-bl[0])**2 + (tl[1]-bl[1])**2)
+    maxHeight = max(int(heightA), int(heightB))
+
+    dst = np.array([
+        [0,0],
+        [maxWidth-1,0],
+        [maxWidth-1, maxHeight-1],
+        [0, maxHeight-1]],
+        dtype = "float32"
+        )
+    matrix = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, matrix, (maxWidth,maxHeight))
+    return warped
+
+@app.post("/deskew")
+async def deskew_image(file: UploadFile = File(...)):
+    contents = await file.read()
+    nparr = np.frombuffer(contents, np.uint8)
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    result = deskew(image)
+    _,buffer = cv2.imencode('.jpg',result)
+
+    return StreamingResponse(
+        io.BytesIO(buffer.tobytes()),
+        media_type="image/jpeg"
+    )
+@app.post("/process")
+async def process_img(file: UploadFile = File(...)):
+    contents = await file.read()   
+    nparr = np.frombuffer(contents, np.uint8)         # Coverting the image into an array with data type as unsigned 8-bit integer 
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)     # Decoding the numpy array into image
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)    # Coverting the image into gryscale image
+    _, buffer = cv2.imencode('.jpg', gray)            # Converts image matrix backs to bytes to send over HTTP
+
+    return StreamingResponse(
+        io.BytesIO(buffer.tobytes()),                 # io.BytesIO() wraps those bytes into a file-like stream
+        media_type = "image/jpeg"                     # StreamingResponse sends that stream back to the frontend (React)
+    )
+
 @app.post("/upload")            # creating an API route that listens for incoming POST requests.
 async def upload_image(file: UploadFile = File(...)):   # async keyword makes the function asynchronous so that server can handle multiplr reuests simultaneously 
     contents = await file.read()   # await tells Python to not block other requests while waiting as reading a file is time taking
